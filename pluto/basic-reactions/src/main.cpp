91671ab428e8f6a168fd23598119ddc97bc21ecd
Aleksander <alex.nuclearboy@gmail.com>
2024-01-26 20:21:07 +0100

    Update modules

--- pluto/basic-reactions/src/main.cpp
+++ pluto/basic-reactions/src/main.cpp
@@ -1,41 +1,62 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <string>
 #include "ReactionSimulation.h"
 #include <TSystem.h>
+#include <TROOT.h>
 
-int main(int argc, char** argv) {
-    // Check for correct number of arguments
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " product1 product2 ..." 
-			<< std::endl;
-        return 1;
-    }
-
-    // Combine final product names and create a filename
-    std::string finalProducts;
-    std::string filename;
+// Combine final product names and create a filename
+void processArguments(int argc, char** argv, 
+					  std::string& finalProducts, 
+					  std::string& filename) {
     for (int i = 1; i < argc; ++i) {
         finalProducts += argv[i];
         finalProducts += (i < argc - 1) ? " " : "";
         filename += argv[i];
     }
+}
 
-    // Initialize ROOT
+int main(int argc, char** argv) {
+    // Check for correct number of arguments
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " product1 product2 ..." 
+				  << std::endl;
+        return 1;
+    }
+    
+    std::string finalProducts;
+    std::string filename;
+    
+    processArguments(argc, argv, finalProducts, filename);
+    
+    // Initialize PLUTO
     TSystem ts;
-    gSystem->Load("libMatrix.so");
-    gSystem->Load("libHist.so");
-    gSystem->Load("libPhysics.so");
-    gSystem->Load("libRIO.so");
-    gSystem->Load("libTree.so");
-    gSystem->Load("libTreeViewer.so");
-    gSystem->Load("$PLUTOSYS/libPluto.so");
-    gSystem->SetIncludePath("-I/home/WASA-software/pluto/src");
+    const char* libraries[] = {"libMatrix.so", "libHist.so", 
+							   "libPhysics.so", "libRIO.so", 
+							   "libTree.so", "libTreeViewer.so", 
+                               "$PLUTOSYS/libPluto.so"};
 
+    for (const auto& lib : libraries) {
+		gSystem->Load(lib);
+        if (gSystem->Load(lib) == -1) {
+            std::cerr << "Failed to load " << lib << std::endl;
+            return 1;
+        }
+    }
+
+    gSystem->SetIncludePath("-I/$PLUTOSYS/src");
+	
     // Perform reaction simulation
     ReactionSimulation react;
-    for (int runindex = 0; runindex < 10; ++runindex) {
-        react.simulate(finalProducts, filename, runindex + 1);
+    
+    for (int runindex = 8; runindex < 10; ++runindex) {
+		std::cout << "Running simulation iteration " 
+				  << runindex + 1 << "/10..." << std::endl;
+		react.simulate(finalProducts, filename, runindex + 1);
     }
+    
+    std::cout << "Simulation completed successfully." << std::endl;
 
     return 0;
 }
