fadbb6a1d0ecc3d42e1caff284f49a8379f976f4
Aleksander <alex.nuclearboy@gmail.com>
2024-01-27 19:00:33 +0100

    Fix the smear function

--- pluto/basic-reactions/src/ReactionSimulation.cpp
+++ pluto/basic-reactions/src/ReactionSimulation.cpp
@@ -6,19 +6,22 @@
 #include <chrono>
 
 ReactionSimulation::ReactionSimulation() {
+    
     // Beam Smearing Setup
+    double sigma = (1.635 - 1.426) / 2; // Половина розмаху діапазону імпульсу
     
     smear = new PBeamSmearing(const_cast<char*>("beam_smear"), 
-							  const_cast<char*>("Beam smearing"));
-    momentumFunc = new TF1(const_cast<char*>("Uniform"), 
-						   const_cast<char*>("1"), 1.426, 1.635);
-	angularFunc = new TF1(const_cast<char*>("Angular"), 
-						  const_cast<char*>("1"), 0, TMath::TwoPi());
+                              const_cast<char*>("Beam smearing"));
+    momentumFunc = new TF1(const_cast<char*>("Uniform"),
+                           const_cast<char*>("1"), 1.426, 1.635);
+    angularFunc = new TF1(const_cast<char*>("Angular"),
+                          const_cast<char*>("gaus"), 
+                          -TMath::Pi(), TMath::Pi());
+    angularFunc->SetParameters(1.0, 0.0, 0.07);
     smear->SetReaction(const_cast<char*>("p + d"));
-    smear->SetMomentumFunction(momentumFunc);    
+    smear->SetMomentumFunction(momentumFunc);
     smear->SetAngularSmearing(angularFunc);
-	makeDistributionManager()->Add(smear);
-    
+    makeDistributionManager()->Add(smear);
 }
 
 ReactionSimulation::~ReactionSimulation() {
@@ -30,27 +33,27 @@ ReactionSimulation::~ReactionSimulation() {
 void ReactionSimulation::simulate(const std::string& finalProducts,
                                   const std::string& filename,
                                   int iter) {
-	
+                                    
     unsigned int seed = static_cast<unsigned int>(
-		   std::chrono::system_clock::now().time_since_epoch().count());
+        std::chrono::system_clock::now().time_since_epoch().count());
     PUtils::SetSeed(seed);
-
-    // Define the output file path
-    std::string outputFile = "${PLUTO_OUTPUT}/pd-" + filename + "-" + 
-							 std::to_string(iter);
-	
-	// Reaction Setup
-    PReaction myReaction(const_cast<char*>("_P1 = 1.5305"), 
-						 const_cast<char*>("p"), const_cast<char*>("d"), 
-						 const_cast<char*>(finalProducts.c_str()), 
-						 const_cast<char*>(outputFile.c_str()), 
-						 1, 0, 0, 0);
     
+    // Define the output file path
+    std::string outputFile = "${PLUTO_OUTPUT}/pd-" + filename + "-" +
+                              std::to_string(iter);
+                              
+    // Reaction Setup
+    PReaction myReaction(const_cast<char*>("_P1 = 1.5305"),
+                         const_cast<char*>("p"), const_cast<char*>("d"),
+                         const_cast<char*>(finalProducts.c_str()),
+                         const_cast<char*>(outputFile.c_str()),
+                         1, 0, 0, 0);
+                         
     try {
         myReaction.Print();
         myReaction.Loop(1000000);
     } catch (const std::exception& e) {
-        std::cerr << "Error during simulation: " << e.what() 
-				  << std::endl;
+        std::cerr << "Error during simulation: " << e.what()
+                  << std::endl;
     }
 }
--- pluto/basic-reactions/src/main.cpp
+++ pluto/basic-reactions/src/main.cpp
@@ -7,9 +7,8 @@
 #include <TROOT.h>
 
 // Combine final product names and create a filename
-void processArguments(int argc, char** argv, 
-					  std::string& finalProducts, 
-					  std::string& filename) {
+void processArguments(int argc, char** argv,std::string& finalProducts,
+                      std::string& filename) {
     for (int i = 1; i < argc; ++i) {
         finalProducts += argv[i];
         finalProducts += (i < argc - 1) ? " " : "";
@@ -17,11 +16,16 @@ void processArguments(int argc, char** argv,
     }
 }
 
+void printLoadedLibraries() {
+    TString listOfLibraries = gSystem->GetLibraries();
+    std::cout << "Loaded libraries: " << listOfLibraries.Data() 
+              << std::endl;
+}
+
 int main(int argc, char** argv) {
-    // Check for correct number of arguments
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " product1 product2 ..." 
-				  << std::endl;
+        std::cerr << "Usage: " << argv[0] << " product1 product2 ..."
+                  << std::endl;
         return 1;
     }
     
@@ -30,33 +34,51 @@ int main(int argc, char** argv) {
     
     processArguments(argc, argv, finalProducts, filename);
     
-    // Initialize PLUTO
-    TSystem ts;
-    const char* libraries[] = {"libMatrix.so", "libHist.so", 
-							   "libPhysics.so", "libRIO.so", 
-							   "libTree.so", "libTreeViewer.so", 
-                               "$PLUTOSYS/libPluto.so"};
-
-    for (const auto& lib : libraries) {
-		gSystem->Load(lib);
-        if (gSystem->Load(lib) == -1) {
-            std::cerr << "Failed to load " << lib << std::endl;
-            return 1;
+    // Initialize ROOT abd PLUTO
+    const char* libraries[] = {
+        "libMatrix.so", "libHist.so", "libPhysics.so",
+        "libRIO.so", "libTree.so", "libTreeViewer.so",
+        "${PLUTOSYS}/libPluto.so"
+        };
+    
+    printLoadedLibraries();
+    
+    bool allLoaded = true;
+    
+    for (const char* library : libraries) {
+        
+        const char* libraryPath = gSystem->DynamicPathName(library, kTRUE);
+        
+        if (!libraryPath) {
+            if (gSystem->Load(library)) {
+                printf("Shared library %s loaded\n", library);
+            } else {
+                printf("Unable to load %s\n", library);
+                allLoaded = false;
+            }
+        } else {
+            printf("Library %s is already loaded at %s\n", 
+                    library, libraryPath);
         }
     }
-
-    gSystem->SetIncludePath("-I/$PLUTOSYS/src");
-	
+    
+    if (allLoaded) {
+        gSystem->SetIncludePath("-I${PLUTOSYS}/src");
+        printf("All libraries loaded successfully.\n");
+    } else {
+        printf("Some libraries failed to load.\n");
+    }
+    
     // Perform reaction simulation
     ReactionSimulation react;
     
-    for (int runindex = 1; runindex < 10; ++runindex) {
-		std::cout << "Running simulation iteration " 
-				  << runindex + 1 << "/10..." << std::endl;
-		react.simulate(finalProducts, filename, runindex + 1);
+    for (int runindex = 0; runindex < 10; ++runindex) {
+        std::cout << "Running simulation iteration "
+                  << runindex + 1 << "/10..." << std::endl;
+        react.simulate(finalProducts, filename, runindex + 1);
     }
     
     std::cout << "Simulation completed successfully." << std::endl;
-
+    
     return 0;
 }
