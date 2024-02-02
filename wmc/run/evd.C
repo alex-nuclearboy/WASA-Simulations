0ac75649ad1520b1d65f64c70b0e95e00d3631e1
Aleksander <alex.nuclearboy@gmail.com>
2024-02-02 10:40:00 +0100

    Fix the paths to libraries

--- wmc/run/evd.C
+++ wmc/run/evd.C
@@ -6,13 +6,13 @@ void evd()
     mg = (TMultiGraph *)gSClient->GetObject("MDC_Event_Display");
     if (!EvDisCan)
     {
-	EvDisCan = new TCanvas("MDC_Event_Display","MDC Event Display",-600,600);
-	mg->Draw("AP");
+        EvDisCan = new TCanvas("MDC_Event_Display","MDC Event Display",-600,600);
+        mg->Draw("AP");
     }
     else
     {
-	EvDisCan->Clear();
-	mg->Draw("P");
+        EvDisCan->Clear();
+        mg->Draw("P");
     }
     gSClient->Start(1);
 }
