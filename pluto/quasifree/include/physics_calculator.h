b78b64e3f5e86b7a03acf078792f8981e9784bfa
Aleksander K <alex.nuclearboy@gmail.com>
2024-02-22 17:53:43 +0100

    Correct the documentation

--- pluto/quasifree/include/physics_calculator.h
+++ pluto/quasifree/include/physics_calculator.h
@@ -1,6 +1,24 @@
+/**
+ * @file physics_calculator.h
+ * @author AK <alex.nuclearboy@gmail.com>
+ * @brief Declaration of PhysicsCalculator class for performing essential 
+ *        physics calculations for simulations.
+ *
+ * Provides static methods for various physics calculations relevant to 
+ * nuclear reaction simulations, including energy, momentum, and 
+ * invariant mass computations, as well as constructing four-vectors. 
+ * Uses ROOT's mathematical libraries for precise and efficient calculations.
+ *
+ * @version 1.0
+ * @date 2024-02-17
+ *
+ * @note Licensed under the GNU General Public License version 3.0 (GPLv3).
+ */
+
 #ifndef PHYSICS_CALCULATOR_H
 #define PHYSICS_CALCULATOR_H
 
+#include "Rtypes.h"
 #include "TLorentzVector.h"
 
 /**
@@ -10,76 +28,83 @@
 class PhysicsCalculator {
 public:
     /**
-     * Calculate the energy of a particle.
-     * @param p Momentum of the particle (GeV/c).
-     * @param m Mass of the particle (GeV/c^2).
-     * @return Energy of the particle (GeV).
+     * Calculates the total energy of a particle given its momentum and mass 
+     * using the relativistic energy-momentum relation.
+     *
+     * @param p Momentum of particle in GeV/c.
+     * @param m Mass of particle in GeV/c^2.
+     * @return Total energy of particle in GeV.
      */
     static Double_t calculateEnergy(Double_t p, Double_t m);
 
     /**
-     * Calculate the invariant mass of two particles, considering one is 
-     * moving (beam) and the other is at rest (target).
-     * @param m1 Mass of the moving particle (GeV/c^2).
-     * @param m2 Mass of the stationary particle (GeV/c^2).
-     * @param p Momentum of the moving particle (GeV/c).
-     * @return Invariant mass of the system (GeV/c^2).
+     * Calculates the invariant mass of two-particle system, 
+     * with one particle moving with momentum p and the other at rest.
+     *
+     * @param m1 Mass of moving particle in GeV/c^2.
+     * @param m2 Mass of stationary particle in GeV/c^2.
+     * @param p Momentum of moving particle in GeV/c.
+     * @return Invariant mass of system in GeV/c^2.
      */
     static Double_t calculateInvariantMass(Double_t m1, Double_t m2, Double_t p);
 
     /**
-     * Calculate the effective mass of a proton in a proton-neutron system.
-     * @param p Momentum of the target proton (GeV/c).
-     * @return Effective proton mass (GeV/c^2).
+     * Calculates the effective mass of proton in neutron-proton system, 
+     * considering internal dynamics within deuteron.
+     *
+     * @param p Relative momentum between neutron and proton in GeV/c.
+     * @return Effective mass of proton in GeV/c^2.
      */
     static Double_t calculateEffectiveProtonMass(Double_t p);
 
     /**
-     * Calculate the effective momentum of a proton.
-     * @param e1 Energy of the beam proton (GeV).
-     * @param e2 Energy of the target proton (GeV).
-     * @param p1 Momentum of the beam proton (GeV/c).
-     * @param p2 Momentum of the target proton (GeV/c).
-     * @param angle Angle between protons (radians).
-     * @param m_p_eff Effective mass of the proton (GeV/c^2).
-     * @return Effective proton momentum (GeV/c).
+     * Calculates effective momentum of proton in proton-proton system for
+     * quasi-elastic scattering simulations.
+     *
+     * @param e1 Energy of the beam proton in GeV
+     * @param e2 Energy of the target proton in GeV.
+     * @param p1 Momentum of the beam proton in GeV/c.
+     * @param p2 Momentum of the target proton in GeV/c.
+     * @param angle Relative angle between two protons in radians.
+     * @param m_p_eff Effective mass of the target proton in the p-n system in GeV/c^2.
+     * @return Effective momentum of proton in GeV/c.
      */
     static Double_t calculateEffectiveProtonMomentum(
         Double_t e1, Double_t e2, Double_t p1, Double_t p2, 
         Double_t angle, Double_t m_p_eff);
 
     /**
-     * Calculate the total momentum of a particle.
-     * @param px X component of momentum (GeV/c).
-     * @param py Y component of momentum (GeV/c).
-     * @param pz Z component of momentum (GeV/c).
-     * @return Total momentum (GeV/c).
+     * Calculates the total momentum of particle from its component vectors.
+     * @param px X-component of momentum in GeV/c.
+     * @param py Y-component of momentum in GeV/c.
+     * @param pz Z-component of momentum in GeV/c.
+     * @return Total momentum of particle in GeV/c.
      */
     static Double_t calculateMomentum(Double_t px, Double_t py, Double_t pz);
 
     /**
-     * Calculate the beta factor of the center-of-mass system.
-     * @param p Momentum of the beam (GeV/c).
-     * @param m1 Mass of the beam particle (GeV/c^2).
-     * @param m2 Mass of the target particle (GeV/c^2).
+     * Calculates the beta factor of center-of-mass system for two-particle collisions.
+     * @param p Momentum of the beam in GeV/c.
+     * @param m1 Mass of the beam particle in GeV/c^2.
+     * @param m2 Mass of the target particle in GeV/c^2.
      * @return Beta of the center-of-mass system.
      */
     static Double_t calculateBetaCM(Double_t p, Double_t m1, Double_t m2);
 
     /**
-     * Calculate the gamma factor of the center-of-mass system.
+     * Calculates the gamma factor of the center-of-mass system.
      * @param beta Beta of the center-of-mass system.
      * @return Gamma of the center-of-mass system.
      */
     static Double_t calculateGammaCM(Double_t beta);
 
     /**
-     * Create a four-vector for a particle.
-     * @param m Mass of the particle (GeV/c^2).
-     * @param p Momentum of the particle (GeV/c), defaults to 0.
-     * @param theta Polar angle (radians), defaults to 0.
-     * @param phi Azimuthal angle (radians), defaults to 0.
-     * @return A TLorentzVector representing the particle.
+     * Creates a four-vector for a particle.
+     * @param m Mass of the particle in GeV/c^2.
+     * @param p Momentum of the particle in GeV/c, defaults to 0.
+     * @param theta Polar angle in radians, defaults to 0.
+     * @param phi Azimuthal angle in radians, defaults to 0.
+     * @return TLorentzVector representing the particle state.
      */
     static TLorentzVector createFourVector(
         Double_t m, Double_t p = 0, Double_t theta = 0, Double_t phi = 0);
